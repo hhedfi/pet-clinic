4d165c3c1857973d5a87fff34dedb210bcc9c37c
Hamza HADFI <hamza.hadfi@drbanner.com>
2019-08-16 23:03:59 +0200

    Implement Interfaces for services . Closes #1.

---
+++ src/main/java/com/learn/springframework/petclinic/services/PetService.java
@@ -0,0 +1,15 @@
+package com.learn.springframework.petclinic.services;
+
+
+import com.learn.springframework.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
